51d8ac6340a2621b70607f481d247f889fe1583b
linyingzhen <277715645@qq.com>
2017-05-27 09:31:56 +0800

    2017052793154

--- js/Test-ES6.js
+++ js/Test-ES6.js
@@ -1,5 +1,14 @@
+    $.ajax({
+        url: "http://redis.o2ofh.com/api.php?m=Apps&c=order&a=checkorder&order_id=123",
+        data: { 'qrcode': qrcode },
+        dataType: 'JSON',
+        cache: false,
+        type: 'POST',
+        success: function(res) {
+            console.log(res)
 
-let {a,b} = {a:1,b:2}
-
-console.log(a)
-
+        },
+        error: function(e) {
+            console.log(e);
+        }
+    });
