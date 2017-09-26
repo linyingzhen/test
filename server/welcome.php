6236e5cc1e463235e0e938f569603fd99b815e75
linyingzhen <277715645@qq.com>
2017-09-26 15:11:04 +0800

    20170926151054

---
+++ server/welcome.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/9/13 0013
+ * Time: 15:54
+ */
+
+
+echo $_POST['name'];
+echo $_POST['email'];
---
+++ server/welcome_get.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/9/13 0013
+ * Time: 15:59
+ */
+
+echo $_GET['name'];
+echo $_GET['email'];
