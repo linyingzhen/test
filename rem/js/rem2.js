01a17850d53e5a0d7e51d33c1f856aea0fe84f34
linyingzhen <277715645@qq.com>
2018-06-29 09:52:33 +0800

    20180629095203

--- rem/js/rem2.js
+++ rem/js/rem2.js
@@ -1,5 +1,5 @@
 (function() {
-    let currClientWidth, fontValue, originWidth;
+    var currClientWidth, fontValue, originWidth;
     originWidth = 750;
     __resize();
     window.addEventListener("resize", __resize, false);
@@ -7,15 +7,15 @@
     function __resize() {
         currClientWidth = document.documentElement.clientWidth;
         if (currClientWidth > 1024) {
-            currClientWidth = 1024
+            currClientWidth = 1024;
         }
         if (currClientWidth > 768 && currClientWidth < 1024) {
-            currClientWidth = 768
+            currClientWidth = 768;
         }
         if (currClientWidth < 320) {
-            currClientWidth = 320
+            currClientWidth = 320;
         }
         fontValue = ((625 * currClientWidth) / originWidth).toFixed(2);
-        document.documentElement.style.fontSize = fontValue + "%"
+        document.documentElement.style.fontSize = fontValue + "%";
     }
 })();
